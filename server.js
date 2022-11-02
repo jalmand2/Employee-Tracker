773e16e0fa15f5c755dd0c464ce492943fe52f41
Jessica Almand <jlalmand27@gmail.com>
2022-11-01 22:27:55 -0600

    Added express middleware

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const logo = require('asciiart-logo');
 const cTable = require ('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// connect to the database
