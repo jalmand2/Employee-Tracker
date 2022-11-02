d992434a7d1445d93df2dbeee8111f2047a373a6
Jessica Almand <jlalmand27@gmail.com>
2022-11-01 22:24:02 -0600

    Finished creating seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,27 @@
 USE company_db;
 
 INSERT INTO department (id, name)
-VALUES ("Engineering"),
-       ("Finance"),
-       ("Legal"),
-       ("Sales");
+VALUES ( 1, "Engineering"),
+       ( 2, "Finance"),
+       ( 3, "Legal"),
+       ( 4, "Sales");
 
 INSERT INTO role (id, title, salary, deparment_id)
-VALUES ("Sales Lead"),
-       ("Salesperson"),
-       ("Lead Engineer"),
-       ("Software Engineer");
-       ("Account Manager");
-       ("Accountant");
-       ("Legal Team Lead");
-       ("Lawyer");
+VALUES ( 1, "Sales Lead", 100000, 4),
+       ( 2, "Salesperson", 80000, 4),
+       ( 3, "Lead Engineer", 150000, 1),
+       ( 4, "Software Engineer", 120000, 1),
+       ( 5, "Account Manager", 160000, 2),
+       ( 6, "Accountant", 125000, 2),
+       ( 7, "Legal Team Lead", 250000, 3),
+       ( 8, "Lawyer", 190000, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("Sales Lead"),
-       ("Salesperson"),
-       ("Lead Engineer"),
-       ("Software Engineer");
-       ("Account Manager");
-       ("Accountant");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("David", "Seagle", 2),
+       ("Lacey", "Daley", 4),
+       ("Jennifer", "Love-Hewitt", 3),
+       ("John", "Deere", 3),
+       ("Daisy", "Buchanan", 1),
+       ("Sheryl", "Crow", 4),
+       ("Daniel", "Radcliffe", 2),
+       ("Samuel", "Jackson", 1);
